c41ac812d69e944fbb13901ab78eb02f5f2437f8
Nolan Lavollée <lavollee@et.esiea.fr>
2024-12-06 07:39:26 +0100

    ajout bouchon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,17 +12,18 @@ export class AppComponent {
   title: string = 'seaIsHuman';
   garbageAlive: boolean = true;
   bottleAlive: boolean = true;
+  corkOnHeart: boolean = true;
 
   killGarbage(): void {
     this.garbageAlive = false;
-    console.log(this.garbageAlive);
-    console.log(this.bottleAlive);
   }
 
   killBottle(): void {
     this.bottleAlive = false;
-    console.log(this.bottleAlive);
-    console.log(this.garbageAlive);
+  }
+
+  uncork(): void {
+    this.corkOnHeart = false;
   }
 
 }
